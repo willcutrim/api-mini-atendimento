7d1f3f7d7a3aa58e9fecab7866952433efa77506
willcutrim <willyam894@gmail.com>
2025-06-07 15:43:15 -0300

    FEAT: Adicionando namespace do webhook do WhatsApp na API

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_restx import Api
-from app.api.routes import ns  # importa o namespace
+from app.api.routes import ns
+from app.api.whatsapp_webhook import whatsapp_bp
 
 def create_app():
     app = Flask(__name__)
     api = Api(app, title="Mini Atendimento IA", version="1.0", description="API com IA e Redis")
     api.add_namespace(ns, path="/mensagens")
+    api.add_namespace(whatsapp_bp, path="/whatsapp-hook")
+
     return app
--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_restx import Api
 from app.api.routes import ns
+from app.api.whatsapp_webhook import whatsapp_bp
 
 def create_app():
     app = Flask(__name__)
     api = Api(app, title="Mini Atendimento IA", version="1.0", description="API com IA e Redis")
     api.add_namespace(ns)
+    api.add_namespace(whatsapp_bp)
     return app
 
 if __name__ == "__main__":
